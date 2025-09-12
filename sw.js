39e8298b9b74f9012cb0a3772b8643fb3b98f05b
6630180REM <kevin.swenson@hotmail.com>
2025-09-12 14:50:34 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 navigator.serviceWorker.register('/firebase-messaging-sw.js')
   .then((registration) => {
-    console.log('Firebase messaging service worker registered:', registration);
+        console.log('Firebase messaging service worker registered:', registration);
   }).catch((error) => {
-    console.error('Firebase messaging service worker registration failed:', error);
+        console.error('Firebase messaging service worker registration failed:', error);
   });
